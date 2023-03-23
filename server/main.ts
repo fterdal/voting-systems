5bedb7d14eb455dfea65bce030f34c4cd36786eb
Ryan Streur <ryan.streur@rsginc.com>
2023-03-23 15:04:01 -0700

    feat(server): add random number route

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
+
+app.get("/random", (req: Request, res: Response) => {
+  const numbah = randomNumber().toString();
+  console.log(`Random number generated: ${numbah}`);
+  res.send(numbah);
+});
+
+function randomNumber() {
+  return Math.floor(Math.random() * 1000);
+}
 
 const PORT = 8080;
 app.listen(PORT, () => {
